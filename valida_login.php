bd063132298966a296572509027ae009ddb6fcc8
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-08-12 22:14:37 -0300

    autenticando usuario

--- valida_login.php
+++ valida_login.php
@@ -1,14 +1,26 @@
 <?php 
-    // print_r($_GET);
+    //variavel que verifica se a autenticacao foi realizada
+    $usuario_autenticado = false;
 
-    // echo "<br>";
+    $usuarios_app = array (
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
 
-    // echo $_GET['email'];
-    // echo $_GET['senha'];
+    // echo '<pre>';
+    // print_r($usuarios_app);
+    // echo '</pre>';
 
-    print_r($_POST);
-    echo "<br>";
-    echo $_POST['email'];
-    echo "<br>";
-    echo $_POST['senha'];
+    foreach($usuarios_app as $user) {
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado) {
+        echo 'autenticado';
+    }
+    else {
+        header('Location: index.php?login=erro');
+    }
 ?>
